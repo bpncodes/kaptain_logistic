dbb49e567c7762a12c75a87d50502a809a0acfee
bpncodes <131p11v@gmail.com>
2024-11-02 18:09:41 -0700

    Add user type to admin users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,8 +4,20 @@ class AdminUser < ApplicationRecord
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
 
+  # Define roles as an enum
+  enum role: { admin: 0, editor: 1, viewer: 2 }
+
+  # You can set a default role for new users if desired
+  after_initialize :set_default_role, if: :new_record?
+
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+  end
+
+  private
+
+  def set_default_role
+    self.role ||= :viewer
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_09_010655) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_03_010401) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_09_010655) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
