af8fc468209b93b7dec2ee2d1339de97f85946eb
bpncodes <131p11v@gmail.com>
2024-11-07 19:30:50 -0800

    Update faker and phone number

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,13 +1,8 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :phone_number, :role
 
   controller do
     before_action :authenticate_admin_user!
-
-    # def index
-      # authorize AdminUser # Ensure user has permission
-    # end
-
   end
 
 
@@ -19,6 +14,7 @@ ActiveAdmin.register AdminUser do
     column :sign_in_count
     column :created_at
     column :role
+    column :phone_number
     actions
   end
 
@@ -30,9 +26,14 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs do
       f.input :email
-      f.input :password
-      f.input :password_confirmation
       f.input :role
+      f.input :phone_number
+            # Only show password fields for new users or when changing the password
+      if f.object.new_record? || f.object.password.present?
+        f.input :password
+        f.input :password_confirmation
+      end
+
     end
     f.actions
   end
